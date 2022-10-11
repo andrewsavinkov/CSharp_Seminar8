a3283b3da78545445cbf36d422b86629463c0357
Andrei Savinkov <andrewsavinkov@gmail.com>
2022-10-11 15:23:28 +0500

    написал код

--- HT2/Program.cs
+++ HT2/Program.cs
@@ -8,3 +8,62 @@
 Программа считает сумму элементов в каждой строке и выдаёт номер строки с наименьшей суммой элементов: 1 строка
 */
 
+int[,] GenerateIntMatrix(int rows, int columns)
+{
+    int[,] inputMatr = new int[rows, columns];
+    for (int i = 0; i < rows; i++)
+    {
+        for (int j = 0; j < columns; j++)
+            inputMatr[i, j] = new Random().Next(0, 11);
+    }
+    return inputMatr;
+}
+
+void PrintMatrix(int[,] inputMatrix)
+{
+    for (int i = 0; i < inputMatrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < inputMatrix.GetLength(1); j++)
+            Console.Write($"{inputMatrix[i, j]} \t");
+        Console.WriteLine();
+    }
+}
+
+int inputMatrSum(int row, int[,] inputMatr)
+{
+    int sum = 0;
+    for (int i = 0; i < inputMatr.GetLength(1); i++)
+    {
+        sum = sum + inputMatr[row, i];
+    }
+    return sum;
+}
+
+int MinimumOfVector(int[] vec)
+{
+    int min = vec[0];
+    int minIndex = 0;
+    for (int i = 0; i < vec.Length; i++)
+    {
+        if (vec[i] < min)
+        {
+            min = vec[i];
+            minIndex = i;
+        }
+    }
+    return minIndex + 1;
+}
+
+int MinimalRow(int[,] matrix)
+{
+    int[] vectorOfSums = new int[matrix.GetLength(0)];
+    for (int i = 0; i < matrix.GetLength(0); i++)
+        vectorOfSums[i] = inputMatrSum(i, matrix);
+    return MinimumOfVector(vectorOfSums);
+}
+
+int[,] testMatrix = GenerateIntMatrix(3, 3);
+PrintMatrix(testMatrix);
+Console.WriteLine();
+int min = MinimalRow(testMatrix);
+Console.WriteLine($"{min} row");
