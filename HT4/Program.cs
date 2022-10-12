921479b3c6477a18616b5c8fbf45d61dbf1a592b
Andrei Savinkov <andrewsavinkov@gmail.com>
2022-10-12 14:44:57 +0500

    написал код

--- HT4/Program.cs
+++ HT4/Program.cs
@@ -6,3 +6,47 @@
 27(0,0,1) 90(0,1,1)
 26(1,0,1) 55(1,1,1)
 */
+int[,,] GenerateThreeDMatrix(int rows, int cols, int depth)
+{
+    int[,,] result = new int[rows, cols, depth];
+    int generatedNumber = 0;
+    Random rnd = new Random();
+    HashSet<int> listOfGeneratedNumbers = new HashSet<int>();
+    for (int i = 0; i < rows; i++)
+    {
+        for (int j = 0; j < cols; j++)
+        {
+            for (int k = 0; k < depth; k++)
+            {
+                do
+                {
+                    generatedNumber = rnd.Next(10, 100);
+                } while (!listOfGeneratedNumbers.Add(generatedNumber));
+                result[i, j, k] = generatedNumber;
+            }
+
+        }
+    }
+    return result;
+}
+
+void PrintMatrixThreeD(int[,,] inputMatrix)
+{
+    for (int i = 0; i < inputMatrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < inputMatrix.GetLength(1); j++)
+        {
+            for (int k = 0; k < inputMatrix.GetLength(2); k++)
+            {
+                Console.Write($"{inputMatrix[i, j, k]}({i},{j},{k}) \t");
+            }
+            Console.WriteLine();
+        }
+    }
+}
+
+int r = 10;
+int c = 2;
+int d = 2;
+int[,,] testMatrix = GenerateThreeDMatrix(r, c, d);
+PrintMatrixThreeD(testMatrix);
