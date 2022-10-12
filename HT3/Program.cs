d55e2088ec97c069303418cad47b03c69cac17b8
Andrei Savinkov <andrewsavinkov@gmail.com>
2022-10-12 11:25:18 +0500

    написал код

--- HT3/Program.cs
+++ HT3/Program.cs
@@ -1,10 +1,12 @@
-﻿/* Задача 58: Задайте две матрицы. Напишите программу, которая будет находить произведение двух матриц.
+﻿/* 
+Задача 58: Задайте две матрицы. Напишите программу, которая будет находить произведение двух матриц.
 Например, даны 2 матрицы:
 2 4 | 3 4
 3 2 | 3 3
 Результирующая матрица будет:
 18 20
-15 18*/
+15 18
+*/
 
 int[,] GenerateIntMatrix(int rows, int columns)
 {
@@ -27,32 +29,62 @@ void PrintMatrix(int[,] inputMatrix)
     }
 }
 
-int[,] MatrixMultiplikation(int[,] matrA, int[,] matrB)
+int[] HorizVectorExtractor(int row, int[,] inputMat)
 {
-    int[,] result = new int[matrA.GetLength(0), matrB.GetLength(1)];
-    for (int i = 0; i < result.GetLength(0); i++)
-    {
-        for (int j = 0; j < result.GetLength(1); j++)
-        {
-            for (int n = 0; n < matrA.GetLength(0); n++)
-            {
-                result[i, j] = result[i, j] + matrA[i, n] * matrB[n, j];
-            }
-        }
-    }
-    return result;
+    int[] resultVector = new int[inputMat.GetLength(1)];
+    for (int i = 0; i < inputMat.GetLength(1); i++)
+        resultVector[i] = inputMat[row, i];
+    return resultVector;
 }
 
+int[] VertVectorExtractor(int col, int[,] inputMat)
+{
+    int[] resultVector = new int[inputMat.GetLength(0)];
+    for (int i = 0; i < inputMat.GetLength(0); i++)
+        resultVector[i] = inputMat[i, col];
+    return resultVector;
+}
 
-int width = 3;
-int height = 3;
-int[,] matrOne=GenerateIntMatrix(height, width);
-int[,] matrTwo=GenerateIntMatrix(height, width);
+int ScalarComposition(int[] vectorOne, int[] vectorTwo)
+{
+    int length = vectorOne.Length;
+    int composition = 0;
+    for (int i = 0; i < length; i++)
+        composition = composition + vectorOne[i] * vectorTwo[i];
+    return composition;
+}
+
+int[,] MatrixMultiplikation(int[,] matrA, int[,] matrB)
+{
+    if (matrA.GetLength(1) == matrB.GetLength(0))
+    {
+        for (int i = 0; i < result.GetLength(0); i++)
+        {
+            for (int j = 0; j < result.GetLength(1); j++)
+            {
+                result[i, j] = ScalarComposition(HorizVectorExtractor(i, matrA), VertVectorExtractor(j, matrB));
+            }
+        }
+        return result;
+    }
+    else
+    {
+        Console.WriteLine("Error: Matrix dimensions must agree!");
+        return result;
+    }
+}
+
+int widthOne = 2;
+int heightOne = 2;
+int widthTwo = 2;
+int heightTwo = 2;
+int[,] matrOne = GenerateIntMatrix(heightOne, widthOne);
+int[,] matrTwo = GenerateIntMatrix(heightTwo, widthTwo);
 
 PrintMatrix(matrOne);
 Console.WriteLine();
 PrintMatrix(matrTwo);
 Console.WriteLine();
 
-int[,] res = MatrixMultiplikation(matrOne, matrTwo);
-PrintMatrix(res);
+int[,] resultingMatrix = MatrixMultiplikation(matrOne, matrTwo);
+PrintMatrix(resultingMatrix);
